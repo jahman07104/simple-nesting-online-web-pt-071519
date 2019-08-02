1c51a5201525e3fa9ecd1393d1d64ceca792b906
Patrick Harrison <jahman@bellatlantic.net>
2019-08-02 18:29:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay][:known_for]
+     programmer_hash[:alan_kay][:known_for]="GUI"
 end
 
 def dennis_ritchies_language
@@ -90,6 +90,7 @@ def adding_matz
 end
 
 def changing_alan
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
