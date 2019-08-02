b7e1645e3cbda39df2baf3fd23dd6e826423f5c6
Patrick Harrison <jahman@bellatlantic.net>
2019-08-02 18:03:13 +0000

    Automatically backed up by Learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -1,5 +1,6 @@
 require_relative "spec_helper"
 require_relative "../nested.rb"
+require "pry"
 
 describe "nested hash" do 
   describe "#hopper" do 
@@ -33,7 +34,9 @@ describe "nested hash" do
     end
   end
 
+
   describe "#adding_to_dennis" do 
+    binding.pry
     it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" do 
       expect(adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
     end
