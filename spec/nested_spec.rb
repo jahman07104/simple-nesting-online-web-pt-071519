851c91de7a3def443ae87403b78756585426cf62
Patrick Harrison <jahman@bellatlantic.net>
2019-08-02 18:30:54 +0000

    Automatically backed up by Learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -29,6 +29,7 @@ describe "nested hash" do
   end
 
   describe "#changing_alan" do 
+    binding.pry
     it "operates on the programmer_hash and changes what Alan Kay is known for, returning the newly-changed hash" do 
       expect(changing_alan[:alan_kay][:known_for]).to eq("GUI")
     end
